e6d6635be97ce1051bc19fd4e8a83e4148696372
triplem <mahdi.triplem@gmail.com>
2025-10-05 12:25:03 +0330

    add app meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui']
-})
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    app: {
+        head: {
+            meta: [{ name: "apple-mobile-web-app-title", content: "TripleM" }],
+            link: [
+                { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+            ],
+        },
+    },
+    modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui"],
+    css: ["~/assets/css/main.css"],
+});
